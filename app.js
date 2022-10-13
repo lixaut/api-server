37374d5ff9fe871cdc3b394a9bc72a25565289e1
wangli <wanglixaut@qq.com>
2022-10-13 17:00:57 +0800

    init router-modeles

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const cors = require('cors')
+const userRouter = require('./routers/user')
 
 const app = express()
 
@@ -9,6 +10,9 @@ app.use(cors())
 
 // 配置解析表单数据的中间件，注意：这个中间件只能解析 application/x-www-form-urlencoded 格式的表单数据
 app.use(express.urlencoded({ extended: false }))
+
+// 使用用户路由模块
+app.use('/api', userRouter)
 
 app.listen(3007, () => {
   console.log('api server running at http://127.0.0.1:3007')
--- routers/user.js
+++ routers/user.js
@@ -1,5 +1,16 @@
 
 const express = require('express')
 
-const router = express.router
+const router = express.Router()
 
+// 注册新用户
+router.post('/reguser', (req, res) => {
+  res.send('reguser OK')
+})
+
+// 登录
+router.post('/login', (req, res) => {
+  res.send('login OK')
+})
+
+module.exports = router
