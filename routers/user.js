5fd9bde67874f89a0e46294107679e96b01ceab7
wangli <wanglixaut@qq.com>
2022-10-13 17:09:05 +0800

    路由模块化

--- routers/user.js
+++ routers/user.js
@@ -1,16 +1,13 @@
 
 const express = require('express')
+const user_handler = require('../router_handler/user')
 
 const router = express.Router()
 
 // 注册新用户
-router.post('/reguser', (req, res) => {
-  res.send('reguser OK')
-})
+router.post('/reguser', user_handler.regUser)
 
 // 登录
-router.post('/login', (req, res) => {
-  res.send('login OK')
-})
+router.post('/login', user_handler.login)
 
 module.exports = router
